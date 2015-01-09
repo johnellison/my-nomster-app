0810d360253caa5929360627a5bf1b4b9b179752
John Ellison <john@john-ellison.com>
2015-01-09 00:53:52 +0000

    added places controller, route, and index action + view

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Nomster::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'places#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'

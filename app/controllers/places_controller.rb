5dfd2ad1255229af18a50d7e2517c040a778ce61
John Ellison <john@john-ellison.com>
2015-01-13 19:42:25 +0000

    added edit action to places controller; added view

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -15,6 +15,14 @@ class PlacesController < ApplicationController
     redirect_to root_path
   end
 
+  def show
+    @place = Place.find(params[:id])
+  end
+
+  def edit
+    @place = Place.find(params[:id])
+  end
+
   private
 
   def place_params

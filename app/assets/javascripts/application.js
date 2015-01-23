079bbfa290602c2214f722441cff61d1e7e72758
John Ellison <john@john-ellison.com>
2015-01-23 19:28:29 +0000

    updated masonry layout and config

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,10 +14,7 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require masonry/jquery.masonry
-//= require masonry/jquery.event-drag
 //= require masonry/jquery.imagesloaded.min
 //= require masonry/jquery.infinitescroll.min
 //= require masonry/modernizr-transitions
-//= require masonry/box-maker
-//= require masonry/jquery.loremimages.min
 //= require_tree .
